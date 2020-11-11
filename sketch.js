27e2ad957ec45a353d86b04f149ed753308afbee
Aadishree2305 <71633621+Aadishree2305@users.noreply.github.com>
2020-11-11 16:28:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas,backgroundImage,database,playerCount;
+var gameState = 0;
+var form,game,player;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database;
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+
+function draw(){
+
+}
